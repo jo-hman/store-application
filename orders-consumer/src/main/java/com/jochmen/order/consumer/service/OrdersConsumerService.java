f4c9511a711e6a033a90b37053790fe177a4e561
ochman <ochmankuba@gmail.com>
2023-11-19 15:28:38 +0100

    minor fixes

--- orders-consumer/src/main/java/com/jochmen/order/consumer/service/OrdersConsumerService.java
+++ orders-consumer/src/main/java/com/jochmen/order/consumer/service/OrdersConsumerService.java
@@ -1,16 +1,20 @@
 package com.jochmen.order.consumer.service;
 
 import com.jochmen.order.consumer.service.message.OrderMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrdersConsumerService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrdersConsumerService.class);
+
     @RabbitListener(queues = "ordersQueue")
     public void listen(OrderMessage orderMessage) {
-        //todo REST call to product module to assign product
-        System.out.println("Message arrived from ordersQueue: " + orderMessage.productIds() + " " + orderMessage.accountId());
+        log.info("New message arrived! Order has been placed for account {} and products {}",
+                orderMessage.accountId(), orderMessage.productIds());
     }
 
 }
